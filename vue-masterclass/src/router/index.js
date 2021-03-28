8e3b281e8f60a3d7cd754d87a47deda9213e1030
Paul Laplante <pllplnt2@gmail.com>
2021-03-28 14:59:19 -0400

    The final result

--- vue-masterclass/src/router/index.js
+++ vue-masterclass/src/router/index.js
@@ -1,5 +1,11 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/pages/Home'
+import ThreadShow from '@/pages/ThreadShow'
+import NotFound from '@/pages/NotFound'
+import Forum from '@/pages/Forum'
+import Category from '@/pages/Category'
+import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from '@/data.json'
+import Profile from '@/pages/Profile'
 
 const routes = [
   {
@@ -8,18 +14,53 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/me',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/category/:id',
+    name: 'Category',
+    component: Category,
+    props: true
+  },
+  {
+    path: '/forum/:id',
+    name: 'Forum',
+    component: Forum,
+    props: true
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: ThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+      // if doesnt exist redirect to not found
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+export default createRouter({
+  history: createWebHistory(),
   routes
 })
-
-export default router
